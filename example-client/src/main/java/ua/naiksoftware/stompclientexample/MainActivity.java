00b93c5d68a695e676665e48e873fdde1f9ea57b
Than Trung Hieu <hieu19926@gmail.com>
2020-02-01 23:23:45 +0700

    send msg ok

---
+++ example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
@@ -0,0 +1,176 @@
+package ua.naiksoftware.stompclientexample;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import io.reactivex.CompletableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.dto.StompHeader;
+import ua.naiksoftware.stomp.StompClient;
+
+import static ua.naiksoftware.stompclientexample.RestClient.ANDROID_EMULATOR_LOCALHOST;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SimpleAdapter mAdapter;
+    private List<String> mDataSet = new ArrayList<>();
+    private StompClient mStompClient;
+    private Disposable mRestPingDisposable;
+    private final SimpleDateFormat mTimeFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+    private RecyclerView mRecyclerView;
+    private Gson mGson = new GsonBuilder().create();
+
+    private CompositeDisposable compositeDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mAdapter = new SimpleAdapter(mDataSet);
+        mAdapter.setHasStableIds(true);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
+
+        mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://" + ANDROID_EMULATOR_LOCALHOST
+                + ":" + RestClient.SERVER_PORT + "/example-endpoint/websocket");
+
+        resetSubscriptions();
+    }
+
+    public void disconnectStomp(View view) {
+        mStompClient.disconnect();
+    }
+
+    public static final String LOGIN = "login";
+
+    public static final String PASSCODE = "passcode";
+
+    public void connectStomp(View view) {
+
+        List<StompHeader> headers = new ArrayList<>();
+        headers.add(new StompHeader(LOGIN, "guest"));
+        headers.add(new StompHeader(PASSCODE, "guest"));
+
+        mStompClient.withClientHeartbeat(1000).withServerHeartbeat(1000);
+
+        resetSubscriptions();
+
+        Disposable dispLifecycle = mStompClient.lifecycle()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            toast("Stomp connection opened");
+                            break;
+                        case ERROR:
+                            Log.e(TAG, "Stomp connection error", lifecycleEvent.getException());
+                            toast("Stomp connection error");
+                            break;
+                        case CLOSED:
+                            toast("Stomp connection closed");
+                            resetSubscriptions();
+                            break;
+                        case FAILED_SERVER_HEARTBEAT:
+                            toast("Stomp failed server heartbeat");
+                            break;
+                    }
+                });
+
+        compositeDisposable.add(dispLifecycle);
+
+        // Receive greetings
+        Disposable dispTopic = mStompClient.topic("/topic/greetings")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(topicMessage -> {
+                    Log.d(TAG, "Received " + topicMessage.getPayload());
+                    addItem(mGson.fromJson(topicMessage.getPayload(), EchoModel.class));
+                }, throwable -> {
+                    Log.e(TAG, "Error on subscribe topic", throwable);
+                });
+
+        compositeDisposable.add(dispTopic);
+
+        mStompClient.connect(headers);
+    }
+
+    public void sendEchoViaStomp(View v) {
+//        if (!mStompClient.isConnected()) return;
+        compositeDisposable.add(mStompClient.send("/topic/hello-msg-mapping", "Echo STOMP " + mTimeFormat.format(new Date()))
+                .compose(applySchedulers())
+                .subscribe(() -> {
+                    Log.d(TAG, "STOMP echo send successfully");
+                }, throwable -> {
+                    Log.e(TAG, "Error send STOMP echo", throwable);
+                    toast(throwable.getMessage());
+                }));
+    }
+
+    public void sendEchoViaRest(View v) {
+        mRestPingDisposable = RestClient.getInstance().getExampleRepository()
+                .sendRestEcho("Echo REST " + mTimeFormat.format(new Date()))
+                .compose(applySchedulers())
+                .subscribe(() -> {
+                    Log.d(TAG, "REST echo send successfully");
+                }, throwable -> {
+                    Log.e(TAG, "Error send REST echo", throwable);
+                    toast(throwable.getMessage());
+                });
+    }
+
+    private void addItem(EchoModel echoModel) {
+        mDataSet.add(echoModel.getEcho() + " - " + mTimeFormat.format(new Date()));
+        mAdapter.notifyDataSetChanged();
+        mRecyclerView.smoothScrollToPosition(mDataSet.size() - 1);
+    }
+
+    private void toast(String text) {
+        Log.i(TAG, text);
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    protected CompletableTransformer applySchedulers() {
+        return upstream -> upstream
+                .unsubscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    private void resetSubscriptions() {
+        if (compositeDisposable != null) {
+            compositeDisposable.dispose();
+        }
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mStompClient.disconnect();
+
+        if (mRestPingDisposable != null) mRestPingDisposable.dispose();
+        if (compositeDisposable != null) compositeDisposable.dispose();
+        super.onDestroy();
+    }
+}
